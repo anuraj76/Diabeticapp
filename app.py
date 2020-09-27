86f5483adf6b1fed0a4a7aec30e142e5db2b83fc
Anuraj Shinde <64351037+anuraj76@users.noreply.github.com>
2020-09-27 23:28:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('DiabeticPredictor.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    Pregnancies = float(request.form['Pregnancies'])
+    SkinThickness = float(request.form['SkinThickness'])
+    Insulin = float(request.form['Insulin'])
+    DiabetesPedigreeFunction = float(request.form['DiabetesPedigreeFunction'])
+    finalFeatures = np.array([[Pregnancies,SkinThickness,Insulin,DiabetesPedigreeFunction]])
+    prediction = model.predict(finalFeatures)
+
+    
+
+    return render_template('index.html', prediction_text='Expected Profit from the Startup is  $ {}'.format(prediction))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
